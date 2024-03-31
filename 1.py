781d1531712fbb8051c6b8b58caea9906045d9c9
nataliashalaeva <112845670+nataliashalaeva@users.noreply.github.com>
2024-03-31 17:10:35 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,10 @@
 #Вариант 4 ЕГЭ-2023.
+with open('27-167b.txt') as f:
+    n, k = map(int, f.readline().split())
+    data = [int(i) for i in f]
+    min1 = min2 = min3 = 10 ** 100
+    for i in range(k * 2, n):
+        min1 = min(min1, data[i - k * 2])
+        min2 = min(min2, data[i - k] * min1)
+        min3 = min(min3, data[i] * min2)
+print(min3 % (10 ** 6 + 1))
